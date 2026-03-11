b50d9a8af0cdd823bb5f8a7a8aafe557a2048c73
Yassir <yassir.hsahraoui@gmail.com>
2026-03-11 23:48:06 +0100

    Version 1

---
+++ Website/src/App.jsx
@@ -0,0 +1,17 @@
+import Login from "./components/Login";
+import Register from "./components/Register";
+import Auth from "./components/Auth";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-[#D1DFEC] font-sans">
+      <div className="bg-white w-[1200px] h-[650px] shadow-2xl  relative rounded-xl border border-[#D1DFEC] overflow-hidden">
+        <Login />
+        <Register />
+        <Auth />
+      </div>
+    </div>
+  );
+}
+
+export default App;
