6f7a23341e46c6328f9ddf58a95bd1665abc8043
Killam22 <malikboukerroum@gmail.com>
2026-04-17 14:30:17 +0100

    otp settings

--- backend/settings.py
+++ backend/settings.py
@@ -170,7 +170,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
